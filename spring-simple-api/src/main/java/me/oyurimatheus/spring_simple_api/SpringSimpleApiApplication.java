576904936d7f3b65e627cda1b05785bef088d9cc
Yuri Matheus <yurimatheusb@hotmail.com>
2024-07-17 16:26:15 -0300

    first commit

---
+++ spring-simple-api/src/main/java/me/oyurimatheus/spring_simple_api/SpringSimpleApiApplication.java
@@ -0,0 +1,13 @@
+package me.oyurimatheus.spring_simple_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleApiApplication.class, args);
+	}
+
+}
---
+++ spring-simple-api/src/test/java/me/oyurimatheus/spring_simple_api/SpringSimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.oyurimatheus.spring_simple_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
